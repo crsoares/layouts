4d42d1bd0dd5079a16b7975439e531f5614cd547
crysthiano <crysthiano.php@hotmail.com>
2014-02-13 12:05:37 -0200

    desenvolvimento layouts

--- layout1.php
+++ layout1.php
@@ -51,7 +51,7 @@
 
      <div class="container">
          <div class="row">
-             <div class="col-md-12 col-sm-6">
+             <div class="col-md-12 col-xs-12">
                  <header class="navbar-fixed-top" style="background-color: #EFF2F5;">
                      <div class="container">
                          <div class="pull-left">
@@ -74,9 +74,9 @@
              </div>
          </div>
          <div class="row">
-             <div class="col-md-2 col-sm-1">
-                 <h4 class="navbar-fixed-top" style="margin-left: 34px; margin-top: 60px;">Menu Principal</h4>
-                 <div class="panel-group" id="accordion">
+             <div class="col-md-2 col-xs-2" style="background-color: #004492; color: #FFF;">
+                 <h4 class="" style="margin-left: 45px; margin-top: 60px;">Menu Principal</h4>
+                 <div class="panel-group" id="accordion" style="">
                      <div class="panel panel-default">
                          <div class="panel-heading">
                              <h4 class="panel-title">
@@ -88,20 +88,22 @@
                          </div>
                          <div id="collapseOne" class="panel-collapse collapse in">
                              <div class="panel-body">
-                                 <table class="table">
-                                     <tr>
-                                         <td>
-                                             <span class="glyphicon glyphicon-pencil text-primary"></span>
-                                             <a href="#">Articles</a>
-                                         </td>
-                                     </tr>
-                                     <tr>
-                                         <td>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <span class="glyphicon glyphicon-pencil text-primary pull-left"></span>
+                                         <a href="#">Articles</a>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
                                              <span class="glyphicon glyphicon-flash text-success"></span>
                                              <a href="#">News</a>
-                                         </td>
-                                     </tr>
-                                 </table>
+                                         </div>
+                                     </div>
+                                 </div>
                              </div>
                          </div>
                      </div>
@@ -116,30 +118,155 @@
                          </div>
                          <div id="collapseThree" class="panel-collapse collapse">
                              <div class="panel-body">
-                                 <table class="table">
-                                     <tr>
-                                         <td>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
                                              <a href="#">Change Password</a>
-                                         </td>
-                                     </tr>
-                                     <tr>
-                                         <td>
-                                             <a href="#">Notifications</a>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
                                              <span class="label label-info">5</span>
-                                         </td>
-                                     </tr>
-                                 </table>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-md-12 col-xs-12" style="border-top: 1px solid #DDDDDD; height: 40px;">
+                                         <div style="padding: 10px 0 0 15px;">
+                                             <a href="#" style="padding-top: 10px;">Notifications</a>
+                                             <span class="label label-info">5</span>
+                                         </div>
+                                     </div>
+                                 </div>
                              </div>
                          </div>
                      </div>
                  </div>
              </div>
-             <div class="col-md-10 col-sm-6">
-                 <nav class="navbar-fixed-top" role="navigation" style="margin-top: 43px; margin-left: 250px;">
-                     <div class="row" style="background-color: #004492; margin-left: 0px;">
-                         <div class="col-md-2" style="border: 1px solid red;">
+             <div class="col-md-10 col-xs-10" style="border: 1px solid red;">
+                 <nav class="navbar-fixed-top" role="navigation" style="margin-top: 43px; margin-left: 228px;">
+                     <div class="row" style="background-color: #004492; margin-left: 0px; padding-bottom: 10px">
+                         <div class="col-md-2">
                              <label>Nome</label>
                              <input type="text" class="form-control input-sm" >
+                         </div>
+                         <div class="col-md-2">
+                             <label>Fabricante</label>
+                             <input type="text" class="form-control input-sm" >
+                         </div>
+                         <div class="col-md-2">
+                             <label>Grupo Produto</label>
+                             <input type="text" class="form-control input-sm" >
+                         </div>
+                         <div class="col-md-2">
+                             <label>SubGrupo Produto</label>
+                             <input type="text" class="form-control input-sm" >
+                         </div>
+                         <div class="col-md-1">
+                             <label>Qtd.</label>
+                             <input type="text" class="form-control input-sm" >
+                         </div>
+                         <div class="col-md-1">
+                             <input style="margin-top: 20px;" type="button" class="btn btn-default" value="Buscar" >
                          </div>
                      </div>
                      <div class="container" style="background-color: #EFF2F5;">
@@ -154,7 +281,7 @@
                                          <ul class="dropdown-menu" role="menu">
                                              <li><a href="http://www.google.com">Novo</a></li>
                                          </ul>
-                                         <div style="margin: -4px 0 0 16px;"><a href="#">Produto</a> <i class="fa fa-plus-square-o fa-lg"></i></div>
+                                         <div style="margin: 0px 0 0 16px;"><a href="#">Produto</a> <i class="fa fa-plus-square-o fa-lg"></i></div>
                                      </a>
 
                                  </div>
@@ -167,15 +294,15 @@
                                          <ul class="dropdown-menu" role="menu">
                                              <li><a href="#">Novo</a></li>
                                          </ul>
-                                         <div style="margin: -4px 0 0 16px;"><a href="#">Produto/Foto</a> <i class="fa fa-plus-square-o fa-lg"></i></div>
+                                         <div style="margin: 0px 0 0 16px;"><a href="#">Produto/Foto</a> <i class="fa fa-plus-square-o fa-lg"></i></div>
                                      </a>
                                  </div>
                                  <div class="btn-group" style="border-right: 1px solid #444; padding: 0 10px 0 4px; height: 15px; margin-right: -4px;">
                                      <a href="#" class="btn" style="border: none;  padding: 0px 2px 6px 0;">
-                                         <div style="margin: -4px 0 0 2px;"><a href="#">Produto/Tabela Preço</a> <i class="fa fa-plus-square-o fa-lg"></i></div></a>
+                                         <div style="margin: 0px 0 0 2px;"><a href="#">Produto/Tabela Preço</a> <i class="fa fa-plus-square-o fa-lg"></i></div></a>
                                  </div>
                                  <div class="btn-group" style="padding: 0 10px 0 4px; height: 15px; margin-right: -4px;">
-                                     <a href="#" class="btn" style="border: none; padding: 0px 2px 6px 0;"><div style="margin: -4px 0 0 2px;"><a href="#">Tabela Preço</a> <i class="fa fa-plus-square-o fa-lg"></i></div></a>
+                                     <a href="#" class="btn" style="border: none; padding: 0px 2px 6px 0;"><div style="margin: 0px 0 0 2px;"><a href="#">Tabela Preço</a> <i class="fa fa-plus-square-o fa-lg"></i></div></a>
                                  </div>
                              </li>
                          </ul>
