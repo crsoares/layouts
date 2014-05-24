2f1beb248923b93b3117269bc65b20aa5184f03a
crysthianophp <crysthianophp@gmail.com>
2014-05-24 12:06:06 -0300

    estudos serviço dia 24

---
+++ layout3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<title>Exemplo modal</title>
+		<meta charset="UTF-8" />
+		<link href="bootstrap/css/bootstrap.css" media="screen" rel="stylesheet" type="text/css" />
+		<script src="bootstrap/js/jquery-1.10.2.min.js"></script>
+		<script src="bootstrap/js/bootstrap.js"></script>
+	</head>
+	<body>
+		<!-- <div class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4>Modal title</h4>
+					</div>
+					<div class="modal-body">
+						<p>Modal body</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class=""></button>
+					</div>
+				</div>
+			</div>
+		</div> -->
+		<div class="form-inline">
+			<input type="text" class="input-sm" />
+			<input type="text" class="input-sm" />
+		</div>
+	</body>
+</html>
